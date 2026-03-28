7504631c3c37fbd110b8a696b0177fec9fb33f81
DEILSON   MENDES <deilsongilmar@gmail.com>
2026-03-28 14:41:25 +0000

    query

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get('/home', (req, res) => {
     res.send("Home Page!");
 });
 
+
+app.get('/me', (req, res) => {
+    console.log(req.query);
+    res.send(`Meu nome é ${req.query.nome}, Moro em ${req.query.cidade}`);
+});
+
 app.listen(1234, () => {
     console.log("Servidor iniciado na porta 1234");
 });
